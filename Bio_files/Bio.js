c4db61f10fc9a32a1f1a97f2018ac0a59052fafa
Yash Nandola <nandolayash@gmail.com>
2022-12-03 00:26:23 +0530

    Adding Everything of Code to WebSite

---
+++ Bio_files/Bio.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.4 local-build-20221014
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Bio_files/BioMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');Widget.onload();fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
